02d649e56d7efe682cbbc377db2280c08515567b
TanushriSV <carlyjulio0919@gmail.com>
2026-08-09 21:33:13 +0530

    Configure ML service for Render

--- MindMate-main/ml_model/app.py
+++ MindMate-main/ml_model/app.py
@@ -150,4 +150,5 @@ def predict():
 # RUN SERVER
 # -------------------------
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
